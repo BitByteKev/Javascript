594b3c26a59889f25622568eb2bf20dfb155d075
BitByteKev <kevincromley2020@gmail.com>
2025-08-03 21:59:56 -0700

    Blog post

---
+++ vite/src/example.js
@@ -0,0 +1,17 @@
+import { initDatabase } from "./db/init.js";
+import { Post } from "./db/models/post.js";
+
+await initDatabase()
+
+const post = new Post({
+    title: "My First Post",
+    author: "Kevin",
+    contents: "This post is stored in a MongoDB database using mongoose.",
+    tags: ["introduction", "first post", "mongoose"]
+});
+
+await post.save()
+
+const posts = await Post.find({});
+console.log(posts);
+
